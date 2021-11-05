8643c658a84d7f66bfb83e0aeda9843d1ebc1f58
Drake Dodson <dwdodson@iastate.edu>
2021-11-05 18:23:42 -0500

    Started on the basic structure of the app

---
+++ 319-course-critic-ui/src/pages/Metrics.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Metrics() {
+    return (
+        <div className='Metrics'>
+            <p>
+                This is my paragraph    
+            </p>            
+        </div>
+    )
+}
+
+export default Metrics
