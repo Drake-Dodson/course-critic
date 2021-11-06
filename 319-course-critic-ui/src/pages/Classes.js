94c0e9f87110b349e4eda85411cd6b320d6f6af5
Drake Dodson <drwdodson@gmail.com>
2021-11-05 21:26:46 -0500

    Changed classes name

---
+++ 319-course-critic-ui/src/pages/Classes.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Classes() { 
+    return (
+        <div className="classes">
+            <h1>
+                Here is where the Class page would go
+            </h1>
+            <p>
+                Hello, is anyone there? I am scared, I don't know where I am. Everything is white
+            </p>
+        </div>
+    )
+}
+
+export default Classes
