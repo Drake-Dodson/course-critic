c4e128f522fec3e7555e9f89f12858e1b5025423
abellizzi1 <abelliz734@gmail.com>
2021-11-07 17:42:53 -0600

    started AssignmentFeed skeleton

---
+++ 319-course-critic-ui/src/AssignmentFeed.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './AssignmentFeed.css'
+
+export default function AssignmentFeed() {
+    return (
+    <html>
+        <head>
+        <title>
+            Assignment Feed
+        </title>
+    </head>
+    <body>
+        <h1>
+            [course name]: Assignments
+        </h1>
+        <div className='asBorder'>
+            <h2>[Assignment name]</h2>
+            <p>Description of assignment goes here</p>
+            <input type="button" id="viewDiscussion" name="viewDiscussion" value="View Discussion"></input>
+        </div>
+    </body>
+</html>
+    )
+}
