12572dd5f3d8fc13f62794cb18293717a92c897f
abellizzi1 <abelliz734@gmail.com>
2021-11-11 21:59:28 -0600

    some changes to both pages

--- 319-course-critic-ui/src/pages/AssignmentDiscussion.js
+++ 319-course-critic-ui/src/pages/AssignmentDiscussion.js
@@ -3,26 +3,56 @@ import './AssignmentFeed.css'
 import {Link} from "react-router-dom";
 
 export default function AssignmentDiscussion() {
+
     return (
         <html>
             <head>
                 <title>Assignment Discussion</title>
             </head>
             <body>
+            <div className='topMargin'>
                 <Link to="/"><button>
                     Back to Assignments
                 </button>
                 </Link>
-                <h1>[Assignment name]: Discussion</h1>
+                <h1 id="aName">[Assignment name]: Discussion</h1>
                 <h2>Recent Posts</h2>
+                </div>
 
                 <div className='asBorder'>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
                 </div>
+                <div className='asBorder'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div className='asBorder'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div className='asBorder'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div className='asBorder'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
+                <div className='asBorder'>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                </div>
 
+                <div className='bottomMargin'>
                 <input type="button" id="createAssignmentPost" name="createAssignmentPost" value="Create Post"></input>
+                </div>
             </body>
         </html>
     )
--- 319-course-critic-ui/src/pages/AssignmentFeed.js
+++ 319-course-critic-ui/src/pages/AssignmentFeed.js
@@ -11,11 +11,54 @@ export default function AssignmentFeed() {
         </title>
     </head>
     <body>
-        <h1>
-            [course name]: Assignments
-        </h1>
+        <div className='center'>
+            <h1>
+                [course name]: Assignments
+            </h1>
+        </div>
+
         <div className='asBorder'>
-            <h2>[Assignment name]</h2>
+            <h2 id="a1">Assignment 1</h2>
+            <p>Description of assignment goes here</p>
+            <Link to="/assignmentdiscussion"><button>
+              View Discussion
+            </button>
+            </Link>
+        </div>
+        <div className='asBorder'>
+            <h2 id="a2">Assignment 2</h2>
+            <p>Description of assignment goes here</p>
+            <Link to="/assignmentdiscussion"><button>
+              View Discussion
+            </button>
+            </Link>
+        </div>
+        <div className='asBorder'>
+            <h2 id="a3">Assignment 3</h2>
+            <p>Description of assignment goes here</p>
+            <Link to="/assignmentdiscussion"><button>
+              View Discussion
+            </button>
+            </Link>
+        </div>
+        <div className='asBorder'>
+            <h2 id="a4">Assignment 4</h2>
+            <p>Description of assignment goes here</p>
+            <Link to="/assignmentdiscussion"><button>
+              View Discussion
+            </button>
+            </Link>
+        </div>
+        <div className='asBorder'>
+            <h2 id="a5">Assignment 5</h2>
+            <p>Description of assignment goes here</p>
+            <Link to="/assignmentdiscussion"><button>
+              View Discussion
+            </button>
+            </Link>
+        </div>
+        <div className='asBorder'>
+            <h2 id="a6">Assignment 6</h2>
             <p>Description of assignment goes here</p>
             <Link to="/assignmentdiscussion"><button>
               View Discussion
