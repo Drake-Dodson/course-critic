4290769ccb084d025bd90719937e791b89b5a574
abellizzi1 <abelliz734@gmail.com>
2021-11-22 21:13:37 -0600

    started passing data

--- 319-course-critic-ui/src/pages/AssignmentDiscussion.js
+++ 319-course-critic-ui/src/pages/AssignmentDiscussion.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './AssignmentFeed.css'
 import {Link} from "react-router-dom";
+import WriteADiscussionPostPage from './WriteADiscussionPostPage';
+
+
 
 export default function AssignmentDiscussion() {
-
     return (
         <html>
             <head>
@@ -18,37 +20,13 @@ export default function AssignmentDiscussion() {
                 <h1 id="aName">[Assignment name]: Discussion</h1>
                 <h2>Recent Posts</h2>
                 </div>
-
-                <div className='asBorder'>
+                <div className='asBorder' id="discussion_posts">
                     <h3>Title1</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
                         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
                 </div>
-                <div className='asBorder'>
-                    <h3>Title2</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                </div>
-                <div className='asBorder'>
-                    <h3>Title3</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                </div>
-                <div className='asBorder'>
-                    <h3>Title4</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                </div>
-                <div className='asBorder'>
-                    <h3>Title5</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                </div>
+                
 
                 <div className='bottomMargin'>
                 <Link to="/creatediscussionpost"><button>
--- 319-course-critic-ui/src/pages/WriteADiscussionPostPage.js
+++ 319-course-critic-ui/src/pages/WriteADiscussionPostPage.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import './AssignmentFeed.css'
 import {Link } from "react-router-dom";
+import { useState } from 'react';
+import AssignmentDiscussion from './AssignmentDiscussion';
 
 export default function WriteADiscussionPostPage() {
+    const postDivs = [];
+function addToPosts() {
+    alert("getposts called");
+    var titleText = document.getElementById("title");
+    var commentText = document.getElementById("comments");
+    postDivs.push(
+        <div className='asBorder'>
+            <h3>{titleText}</h3>
+            <p>{commentText}</p>
+        </div>
+    )
+}
+
     return (
+        <html>
         <div className='leftMargin'>
             <h1><u>Write a Discussion Post-Assignment Name</u></h1>
  
             <form>
                 <label for="title">Title:</label><br/>
-                <input type="text" id="title" name="title" value="Title of Post"/><br/>
+                <input type="text" id="title" name="title"/><br/>
                 <label for="comments">Comments:</label><br/>
-                <input type="text" id="comments" name="comments" value="Enter Comments About Class Here"/><br/>
-                <input type="submit" value="Submit"/>
+                <input type="text" id="comments" name="comments"/><br/>
+                <button onClick={addToPosts}>Submit</button>
             </form> 
 
             <Link to="/assignmentdiscussion"><button>
-              Back to Assignment Discussion
-            </button>
-            </Link>
+                    Back to Assignment Discussion
+                </button>
+                </Link>
+                
 
         </div>
+        </html>
     )
 }
