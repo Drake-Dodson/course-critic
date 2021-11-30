f421247a2b4c9dcbe6f9627792f35044e44b2911
Sinnwell <mattds@iastate.edu>
2021-11-29 20:14:43 -0600

    726 formatted Write A Discussion Post Page and linked button to Assignment Discussion Page

--- 319-course-critic-ui/src/pages/WriteADiscussionPostPage.js
+++ 319-course-critic-ui/src/pages/WriteADiscussionPostPage.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 export default function WriteADiscussionPostPage() {
     return (
         <div className="WriteADiscussionPostPage">
-            <h1><u>Write a Discussion Post-Assignment Name</u></h1>
+            <h1><u>Write A Discussion Post-Assignment Name</u></h1>
  
             <form>
                 <label for="title"><b>Title:</b></label><br/>
-                <input type="text" id="title" size="150" name="title" value="Title of Class"/><br/><br></br>
+                <input type="text" id="title" size="103" name="title" placeholder="Title of Class"/><br/><br></br>
                 <label for="comments"><b>Comments:</b></label><br/>
-                <input type="text" id="comments" maxlength="1000" name="comments" value="Enter Comments About Class Here"/><br/>
+                <textarea rows= "25" cols="100" id="comments" maxlength="1000" name="comments"placeholder="Enter Comments About Class Here"></textarea><br/>
                 <input type="submit" value="Submit"/>
             </form><br></br> 
 
-            <button type="button" onclick="alert('Hello world!')">Back to Assignment Discussion</button>
+            <div>
+                <Link to="/AssignmentDiscussion"><button>
+                    Back to Assignments Discussion
+                </button>
+                </Link>
+            </div>
 
         </div>
     )
