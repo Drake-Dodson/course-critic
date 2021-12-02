f2e2d918d21a63d08ca07ff2a867c8b32c7617d0
malakhib <malakhib@iastate.edu>
2021-12-02 17:20:30 -0600

    Home page complete.. Awaiting full functionality

--- 319-course-critic-ui/src/pages/Home.js
+++ 319-course-critic-ui/src/pages/Home.js
@@ -1,13 +1,70 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
     return (
         <div className='home'>
-            <h1>
-                Here is where the home screen would go
-            </h1>
+
+            <body>
+                <div class="content">
+                    <form id="Main Page" action="" >
+
+                        <div class="squareone">
+
+                            <p>Course</p>
+
+                        </div>
+                        <div class="squaretwo">
+
+                            <p>Course</p>
+
+                        </div>
+                        <div class="squarethree">
+
+                            <p>Course</p>
+
+                        </div>
+                        <div class="squarefour">
+
+                            <p>Course</p>
+
+                        </div>
+                        <div class="squarefive">
+
+                            <p>Course</p>
+
+                        </div>
+                        <div class="squaresix">
+
+                            <p>Course</p>
+
+                        </div>
+                        <div class="feedbacksquare">
+
+                            <p>Recent Feedback</p>
+
+                        </div>
+                        <div class="showallsquare">
+                            <p>Show All</p>
+                        </div>
+
+                        <div class="notificationtextone">
+                            <p>Notification</p>
+                        </div>
+                        <div class="notificationtexttwo">
+                            <p>Notification</p>
+                        </div>
+                        <div class="notificationtextthree">
+                            <p>Notification</p>
+                        </div>
+
+                    </form>
+                </div>
+
+            </body>
         </div>
     )
 }
 
 export default Home
+
