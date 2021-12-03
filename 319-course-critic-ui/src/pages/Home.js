9cdaa92093e03f6498a6866d35805199fab4de78
malakhib <malakhib@iastate.edu>
2021-12-02 18:25:42 -0600

    Update Home.js

--- 319-course-critic-ui/src/pages/Home.js
+++ 319-course-critic-ui/src/pages/Home.js
@@ -6,62 +6,61 @@ function Home() {
         <div className='home'>
 
             <body>
-                <div class="content">
-                    <form id="Main Page" action="" >
-
-                        <div class="squareone">
-
-                            <p>Course</p>
-
-                        </div>
-                        <div class="squaretwo">
-
-                            <p>Course</p>
-
-                        </div>
-                        <div class="squarethree">
-
-                            <p>Course</p>
-
-                        </div>
-                        <div class="squarefour">
-
-                            <p>Course</p>
-
-                        </div>
-                        <div class="squarefive">
-
-                            <p>Course</p>
-
-                        </div>
-                        <div class="squaresix">
-
-                            <p>Course</p>
-
-                        </div>
-                        <div class="feedbacksquare">
-
-                            <p>Recent Feedback</p>
-
-                        </div>
-                        <div class="showallsquare">
-                            <p>Show All</p>
-                        </div>
-
-                        <div class="notificationtextone">
-                            <p>Notification</p>
-                        </div>
-                        <div class="notificationtexttwo">
-                            <p>Notification</p>
-                        </div>
-                        <div class="notificationtextthree">
-                            <p>Notification</p>
-                        </div>
-
-                    </form>
-                </div>
-
-            </body>
+	<div class="content">
+ <form id="Main Page" action="" >
+   
+     <div class="squareone">
+	 
+	 <p>Course</p>
+	 
+	 </div>
+	 <div class="squaretwo">
+	 
+	 <p>Course</p>
+	 
+	 </div>
+	 <div class="squarethree">
+	 
+	 <p>Course</p>
+	 
+	 </div>
+	 <div class="squarefour">
+	 
+	 <p>Course</p>
+	 
+	 </div>
+	 <div class="squarefive">
+	 
+	 <p>Course</p>
+	 
+	 </div>
+	 <div class="squaresix">
+	 
+	 <p>Course</p>
+	 
+	 </div>
+	   <div class="feedbacksquare">
+	 </div>
+	 <div class="feedbacksquaretext">
+	 
+			<p>Recent Feedback</p>
+	 
+	 </div>
+	
+	  <div class="notificationtextone">
+	 <p>Notification</p>
+	 </div>
+	  <div class="notificationtexttwo">
+	 <p>Notification</p>
+	 </div>
+	  <div class="notificationtextthree">
+	 <p>Notification</p>
+	 </div>
+	 
+  </form>
+  </div>
+  
+</body>
         </div>
     )
 }
