eb336a304b8128d31fc23d47b9c8a6c6970d70a3
Drew Stone <ajstone@iastate.edu>
2021-12-03 13:00:52 -0600

    Upload New File

---
+++ 319-course-critic-ui/src/pages/MainFeed.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './MainFeed.css'
+
+export default function MainFeed()
+{
+    return (
+        <html>
+            <head>
+            <title>
+                MainFeed
+            </title>
+            </head>
+            
+            <body>
+                <div className='center_text'>
+                    <h1>
+                        Main Feed
+                    </h1>
+                </div>
+
+                <div className='course1'>
+                <h2 id="a1">Course</h2>
+                </div>
+                <div className='course2'>
+                <h2 id="a1">Course</h2>
+                </div>
+                <div className='course3'>
+                <h2 id="a1">Course</h2>
+                </div>
+                <div className='course4'>
+                <h2 id="a1">Course</h2>
+                </div>
+                <div className='course5'>
+                <h2 id="a1">Course</h2>
+                </div>
+                <div className='course6'>
+                <h2 id="a1">Course</h2>
+                </div>
+             
+               
+            </body>
+        </html>
+    )
+}
