b055cb2320c96ee8ad62f41d02e944bd30956c64
Malakhi <malakhib@iastate.edu>
2021-12-06 21:01:11 -0600

    Classes is correct now.

--- 319-course-critic-ui/src/pages/Classes.js
+++ 319-course-critic-ui/src/pages/Classes.js
@@ -1,50 +1,38 @@
 import React from 'react'
 import './Classes.css'
-import {Link, useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Classes() {
-    let {classID} = useParams();
+	let { classID } = useParams();
 
-    return (
-        <div className="classes">
-            <body>
-	<div class="content">
- <form id="Main Course View" action="" >
-   
-    <div class="titleunderline"></div>
-	<div class="title">
-		<p>COM S 319</p>
-	</div>
-	<div class="classscore">
-		<p>Class Score:</p>
-	</div>
-	<div class="classscoredata">
-		<p id="ClassScoreData">4.7</p>
-	</div>
-	<div class="courselinksmainbox">
-		<p id="Course Assigments" onclick="" >Assignments</p>
-		<p><br/><br/>Metrics</p>
-        <p>
-		<Link to={"/class/" + classID + "/reviews"}>
-                <br/><br/><br/>reviews
-            </Link>
-            </p>
-	</div>
-	<div class="courselinksheaderbox">
-		<p>Course Links</p>
-	</div>
-	<div class="coursediscriptionbox">
-		<p>Discription:<br/>-insert here-</p>
-	</div>
-	 
-	 
-  </form>
-  </div>
-  
-</body>
+	return (
+		<div className="classes">
 
-        </div>
-    )
+			<div class="titleunderline">
+			</div>
+			<div class="title">
+				<p>COM S 319</p>
+			</div>
+			<div class="classscore">
+				<p>Class Score:</p>
+			</div>
+			<div class="classscoredata">
+				<p id="ClassScoreData">4.7</p>
+			</div>
+			<div class="courselinksmainbox">
+				<p1>Assignments</p1>
+				<p2>Metrics</p2>
+				<p3>Reviews</p3>
+			</div>
+			<div class="courselinksheaderbox">
+				<p>Course Links</p>
+			</div>
+			<div class="coursediscriptionbox">
+				<p>Discription:<br />-insert here-</p>
+			</div>
+
+		</div>
+	)
 }
 
 export default Classes
